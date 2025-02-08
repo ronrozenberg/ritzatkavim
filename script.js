83f1e13e5184caaa064fc8a359ec9cbd37004dd9
ronrozenberg <ronroz10@gmail.com>
2025-02-08 16:42:35 +0200

    attribution deleted

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-        const map = L.map('map', { zoomControl: false }).setView([0, 0], 2);
+        const map = L.map('map', { zoomControl: false }).setView([0, 0], 2, );
+        map.attributionControl.setPrefix(false);
         let userMarker;
         let randomMarker;
         let userPosition;
@@ -8,7 +9,7 @@
         
         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             maxZoom: 19,
-            attribution: '© OpenStreetMap contributors'
+            attribution: 'OpenStreetMap contributors © '
         }).addTo(map);
 
         function showStatus(message) {
